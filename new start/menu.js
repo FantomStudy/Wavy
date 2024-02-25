2d793e7b0872119805c00da0bd8e8432f7e3e772
FantomStudy <144556354+FantomStudy@users.noreply.github.com>
2024-02-25 11:50:52 +0500

    Delete new start directory

--- new start/menu.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector(".burger");
-
-const offScreenMenu = document.querySelector(".off-screen-menu");
-
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
-});
